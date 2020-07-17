27600dc9a496b384d5bcf51bbd1d32ec049f4a83
Ivan Kozik <ivan@ludios.org>
2020-07-17 00:44:43 +0000

    Demonstrate it a little better

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::env;
 use sqlx::postgres::PgPoolOptions;
+use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     // Create a connection pool
     let pool = PgPoolOptions::new()
         .max_connections(2)
+        .connect_timeout(Duration::from_secs(3))
         .connect(&env::var("DATABASE_URL").unwrap()).await?;
 
     let query = "blah blah"; // SELECT 1 works fine
@@ -20,7 +22,9 @@ async fn main() -> Result<(), sqlx::Error> {
     assert_eq!(result.err().unwrap().to_string(), "error returned from database: syntax error at or near \"blah\"");
     drop(tx);
 
+    println!("hanging for 3 seconds now");
     let mut tx = pool.begin().await?;
+    println!("not reached");
     let result = sqlx::query(query).execute(&mut tx).await;
     assert_eq!(result.err().unwrap().to_string(), "error returned from database: syntax error at or near \"blah\"");
     drop(tx);
