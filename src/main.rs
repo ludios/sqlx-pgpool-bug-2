81b4fd59a1c56ec70a1d9a2bd5c6d64a27f3edb2
Ivan Kozik <ivan@ludios.org>
2020-07-18 02:21:31 +0000

    Make it work

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,57 @@
-use std::env;
-use sqlx::Executor;
-use sqlx::postgres::PgPoolOptions;
-use std::time::Duration;
-use tokio::task;
+fn main() {}
 
-#[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
-    let outer_pool = PgPoolOptions::new()
-        .max_connections(10)
-        .after_connect(|conn| Box::pin(async move {
-            conn.execute("SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;").await?;
-            Ok(())
-        }))
-        .connect_timeout(Duration::from_secs(10))
-        .connect(&env::var("DATABASE_URL").unwrap()).await?;
+#[cfg(test)]
+pub(crate) mod tests {
+    use std::env;
+    use anyhow::Result;
+    use sqlx::postgres::{PgPoolOptions, PgPool};
+    use std::time::Duration;
+    use once_cell::sync::Lazy;
+    use std::future::Future;
+    use std::pin::Pin;
+    use futures::future::{FutureExt, Shared};
 
-    let setup = ["
-        CREATE TABLE demo (a int PRIMARY KEY, b int, c int, d int);
-    ", "
-        CREATE OR REPLACE FUNCTION raise_exception() RETURNS trigger AS $$
-        DECLARE
-            message text;
-        BEGIN
-            message := TG_ARGV[0];
-            RAISE EXCEPTION '%', message;
-        END;
-        $$ LANGUAGE plpgsql;
-    ", "
-        CREATE TRIGGER demo_check_update
-            BEFORE UPDATE ON demo
-            FOR EACH ROW
-            EXECUTE FUNCTION raise_exception('cannot update');
-    "];
-
-    let mut tx = outer_pool.begin().await?;
-    for stmt in &setup {
-        sqlx::query(stmt).execute(&mut tx).await?;
+    pub async fn new_pgpool(uri: &str, max_connections: u32) -> PgPool {
+        PgPoolOptions::new()
+            .connect_timeout(Duration::from_secs(10))
+            .max_connections(max_connections)
+            .connect(&uri).await.unwrap()
     }
-    tx.commit().await?;
 
-    let mut handles = vec![];
-    for i in 0..30 {
-        let pool = outer_pool.clone();
-        let handle = task::spawn(async move {
-            for (column, value) in &[
-                ("b", "2"),
-                ("c", "3"),
-                ("d", "4"),
-            ] {
-                let mut tx = pool.begin().await.unwrap();
-                // doesn't always fail because row may not exist
-                let query = format!("UPDATE demo SET {} = {} WHERE a = $1", column, value);
-                let result = sqlx::query(&query).bind(i).execute(&mut tx).await;
-                assert_eq!(result.err().expect("expected an error").to_string(), "error returned from database: cannot update");
-            }
-        });
-        handles.push(handle);
+    /// PgPool Future initialized once by the first caller
+    static POOL: Lazy<Shared<Pin<Box<dyn Future<Output=PgPool> + Send>>>> = Lazy::new(|| async {
+        let uri = env::var("DATABASE_URL").expect("need DATABASE_URL");
+        new_pgpool(&uri, 100).await
+    }.boxed().shared());
 
-        let pool = outer_pool.clone();
-        let handle = task::spawn(async move {
-            let query = "INSERT INTO demo VALUES ($1::int)";
-            let mut tx = pool.begin().await.unwrap();
-            sqlx::query(query).bind(i).execute(&mut tx).await.unwrap();
-            tx.commit().await.unwrap();
-
-            // Generate an error
-            let query = "UPDATE demo SET a = $1 + 10000 WHERE a = $1";
-            let msg = "error returned from database: cannot update";
-            let mut tx = pool.begin().await.unwrap();
-            let result = sqlx::query(query).bind(i).execute(&mut tx).await;
-            assert_eq!(result.err().unwrap().to_string(), msg);
-        });
-        handles.push(handle);
+    /// Return the PgPool for tests
+    async fn pool() -> PgPool {
+        POOL.clone().await
     }
-    futures::future::join_all(handles).await;
 
-    Ok(())
+    #[tokio::test]
+    async fn test_a() -> Result<()> {
+        let pool = pool().await;
+
+        let mut tx = pool.begin().await?;
+        sqlx::query("SELECT * FROM pg_index").execute(&mut tx).await?;
+
+        let mut tx = pool.begin().await?;
+        sqlx::query("SELECT * FROM pg_index").execute(&mut tx).await?;
+
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_b() -> Result<()> {
+        let pool = pool().await;
+
+        for _ in 1..4 {
+            let mut tx = pool.begin().await?;
+            let result = sqlx::query("blah blah").execute(&mut tx).await;
+            assert_eq!(result.err().expect("expected an error").to_string(), "error returned from database: syntax error at or near \"blah\"");
+        }
+
+        Ok(())
+    }
 }
